82f95d7438814284bcb4cac11b79d0fd69ddb56f
dsbraydev <dsbray.dev@gmail.com>
2023-12-10 13:55:53 +0200

    added stats components and fetched the data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Dashboard from "./components/Dashboard";
+// import useData from "./hooks/useData";
 
 const HomePage = () => {
+  // const { data, isLoading, isError } = useData("/profile");
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
+
+  // if (isError) {
+  //   return <div>Error loading data</div>;
+  // }
   return <Dashboard />;
 };
 
